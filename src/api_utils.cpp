9d8bdc98025b6d35ca81a09e67c1354eb87ace40
Chao Kong <kongchao1998@gmail.com>
2025-12-26 20:30:03 +0800

    add default diag to upper wrapper

--- src/api_utils.cpp
+++ src/api_utils.cpp
@@ -25,18 +25,19 @@ DataFrame generate_default_cost_matrix(const DataFrame &data, const String &deli
 
 //' Convert long table to square form
 //'
-//' Convert a distance dataframe in long table form to a square matrix form.
+//' Convert a distance dataframe in long table form to a square matrix form. Values in the diagonal positions are automatically filled with `default_diag`. If you want self-defined diagonal values, define them in the long table.
 //'
 //' @param data Dataframe in long table form. The first and second columns are labels and the third column stores the distance values.
 //' @param symmetric Whether the distance matrix is symmetric (if cost matrix is not, then the distance matrix is also not).
+//' @param default_diag The default value to fill in the diagonal positions. By default it is 0.0.
 //' @return Dataframe in square matrix form, rownames and colnames are labels. If the long table only contains \eqn{C_n^2} rows and `symmetric` is set to FALSE, then only lower triangle positions in the result are filled.
 //' @examples
 //' data <- as.data.frame(list(chars1=c("a","a","b"),chars2=c("b","c","c"),dist=c(1,2,3)))
 //' mat <- long2squareform(data)
 //[[Rcpp::export]]
-DataFrame long2squareform(const DataFrame &data, bool symmetric = true)
+DataFrame long2squareform(const DataFrame &data, bool symmetric = true, double default_diag = 0.0)
 {
-    return lingdist::long2squareform(data, symmetric);
+    return lingdist::long2squareform(data, symmetric, default_diag);
 }
 
 //' Check which symbols in `data` are missing from a cost matrix
