3250203417f72024fd45791184f57401a438b01e
Chao Kong <kongchao1998@gmail.com>
2025-12-26 20:42:17 +0800

    bug fix in long2squareform diag value logic

--- src/utils/helpers.cpp
+++ src/utils/helpers.cpp
@@ -144,7 +144,7 @@ namespace lingdist
                 col2[char2idx[char1]] = dist;
             }
         }
-        for (int i = 0; i < nrow; i++)
+        for (int i = 0; i < unique_chars.size(); i++)
         {
             NumericVector col = result[i];
             if (NumericVector::is_na(col[i]))
