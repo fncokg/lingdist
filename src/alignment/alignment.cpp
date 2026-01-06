11108d167f1d52378d8e02215e432873c626367f
Chao Kong <kongchao1998@gmail.com>
2026-01-06 14:26:11 +0800

    fix alignment bug: index start in get_string_alignment and alignments declaration in get_string_alignment_result

--- src/alignment/alignment.cpp
+++ src/alignment/alignment.cpp
@@ -5,21 +5,26 @@ using namespace Rcpp;
 
 namespace lingdist
 {
+    // dist mat -> id pairs
     // DFS to enumerate all matched optimal paths
     std::vector<std::vector<Point>> get_matched_paths(std::vector<std::vector<DistPts>> dist, int max_paths)
     {
         int nrow = static_cast<int>(dist.size());
         int ncol = static_cast<int>(dist[0].size());
-        int this_x = nrow - 1, this_y = ncol - 1;
+
         std::vector<std::vector<Point>> result;
+        // A stack to store the points to explore and a path from start to the point
         std::vector<std::pair<Point, std::vector<Point>>> stack;
-        std::vector<Point> this_path;
+        std::vector<Point> curr_path;
+
+        // Now we start from the bottom-right corner
+        int curr_x = nrow - 1, curr_y = ncol - 1;
         while (true)
         {
-            if (this_x == 0 && this_y == 0)
+            if (curr_x == 0 && curr_y == 0)
             {
-                this_path.insert(this_path.begin(), std::make_pair(this_x, this_y));
-                result.push_back(this_path);
+                curr_path.insert(curr_path.begin(), std::make_pair(curr_x, curr_y));
+                result.push_back(curr_path);
                 if (stack.empty())
                 {
                     break;
@@ -28,20 +33,20 @@ namespace lingdist
                 {
                     auto stack_back = stack.back();
                     stack.pop_back();
-                    this_x = stack_back.first.first;
-                    this_y = stack_back.first.second;
-                    this_path = stack_back.second;
+                    curr_x = stack_back.first.first;
+                    curr_y = stack_back.first.second;
+                    curr_path = stack_back.second;
                 }
             }
-            this_path.insert(this_path.begin(), std::make_pair(this_x, this_y));
-            Points next_points = dist[this_x][this_y].second;
-            this_x = next_points[0].first;
-            this_y = next_points[0].second;
+            curr_path.insert(curr_path.begin(), std::make_pair(curr_x, curr_y));
+            Points next_points = dist[curr_x][curr_y].second;
+            curr_x = next_points[0].first;
+            curr_y = next_points[0].second;
             if (next_points.size() > 1)
             {
                 for (std::size_t i = 1; i < next_points.size(); i++)
                 {
-                    stack.push_back(std::make_pair(next_points[i], this_path));
+                    stack.push_back(std::make_pair(next_points[i], curr_path));
                     if (static_cast<int>(result.size()) >= max_paths)
                     {
                         break;
@@ -52,26 +57,28 @@ namespace lingdist
         return result;
     }
 
+    // id pairs -> alignment result with strings
     AlignmentResult get_string_alignment_result(const StrVec &chars1, const StrVec &chars2, const CostTable &cost, int max_paths)
     {
         AlignmentResult report;
         std::string char1, char2, opt;
-        int this_x, this_y, prev_x, prev_y;
+        int curr_x, curr_y, prev_x, prev_y;
         auto dist = edit_dist_core_dp_record(chars1, chars2, cost); // Original line
         auto paths = get_matched_paths(dist, max_paths);
         double result = dist[chars2.size()][chars1.size()].first;
         report.distance = result;
-        std::vector<std::pair<std::string, std::string>> alignments;
+
         for (auto &path : paths)
         {
+            std::vector<std::pair<std::string, std::string>> alignments;
             for (std::size_t j = 1; j < path.size(); j++)
             {
-                this_x = path[j].first;
-                this_y = path[j].second;
+                curr_x = path[j].first;
+                curr_y = path[j].second;
                 prev_x = path[j - 1].first;
                 prev_y = path[j - 1].second;
-                char1 = this_y == prev_y ? EMPTY : chars1[this_y - 1];
-                char2 = this_x == prev_x ? EMPTY : chars2[this_x - 1];
+                char1 = curr_y == prev_y ? EMPTY : chars1[curr_y - 1];
+                char2 = curr_x == prev_x ? EMPTY : chars2[curr_x - 1];
                 alignments.emplace_back(std::make_pair(char1, char2));
             }
             report.alignments.emplace_back(alignments);
@@ -79,6 +86,7 @@ namespace lingdist
         return report;
     }
 
+    // format alignment result as R List
     List get_string_alignment(const StrVec &chars1, const StrVec &chars2, const CostTable &cost, int max_paths)
     {
         List report;
@@ -90,7 +98,7 @@ namespace lingdist
         {
             StringVector chars1_col, chars2_col, operation_col;
             NumericVector cost_col, cumcost_col;
-            for (std::size_t j = 1; j < path.size(); j++)
+            for (std::size_t j = 0; j < path.size(); j++)
             {
                 char1 = path[j].first;
                 char2 = path[j].second;
